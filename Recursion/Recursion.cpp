23227703f4659a384ab4721992659dd567d104c5
pavel-romanov-91 <101830399+pavel-romanov-91@users.noreply.github.com>
2022-04-19 10:26:27 +0300

    Recursion

--- Recursion/Recursion.cpp
+++ Recursion/Recursion.cpp
@@ -13,13 +13,32 @@ void elevator(int floor)
 	cout << "Вы на " << floor << "этаже\n ";
 }
 
+double power(double base, int exponent)
+{
+	/*if (exponent == 0) return 1;
+	else if (exponent > 0) return base * power(base, exponent - 1);
+	else if (exponent < 0) return 1 / power(base, exponent + 1);*/
+	//return exponent == 0 ? 1 : exponent > 0 ? power(base, exponent - 1) : 1 / base * power(base, exponent + 1);
+	return exponent == 0 ? 1 : exponent > 0 ? base * power(base, exponent - 1) : 1 / power(base, -exponent);
+}
+
 void main()
 {
 	setlocale(LC_ALL, "");
 	//cout << "Hello World!";
 	//main();
 
-	int n;
+	/*int n;
 	cout << "Введите номер этажа: "; cin >> n;
-	elevator(n);
+	elevator(n);*/
+
+	int number; 
+	int degree;
+	cout << "Введите число: "; cin >> number;
+	cout << "Введите cтепень: "; cin >> degree;
+
+	cout << "Введённое число, в ведённой вами степени равно " << power(number, degree) << endl;
+	main();
+	
+
 }
